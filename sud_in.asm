2a9796809d0a0d74294ed4f7bdcd667a026e214c
Katona Áron <katonaaron01@gmail.com>
2019-06-15 14:03:59 +0300

    Update sud_in.asm
    -The number of bytes read are checked
    -Comments were added
    -don't backup AX before CLOSEFILE

--- sud_in.asm
+++ sud_in.asm
@@ -16,36 +16,40 @@ IN_CODE SEGMENT PARA PUBLIC 'CODE'
 	;Input: DS:DX = Address of the path to the input file. The path is a string terminated in 0.
 	;		DS:BX = Address of the memory location where the read data will be stored.
 	;				Its size is MSIZE, defined in "sud_ct.mac".
+	;Output: AX = ESUCC on success, AX = an error code from "sud_ct.mac" on error
 	READ PROC FAR
-		PUSH DX
+		PUSH DX				;Backup the registers that are used
 		PUSH BX
 		
-		;Open the file
-		FOPEN
+		FOPEN				;Open the file
 		
-		JNC READFILE
-		MOV AX,EFOPEN
-		JMP RETURN
+		JNC READFILE		;Check the CF for error
+		MOV AX,EFOPEN		;An error has occured on opening the file. Assign the corresponding error code
+		JMP RETURN			;Jump to the last instructions
 		
 		READFILE:
-		MOV DX,BX
-		MOV BX,AX
-		FREAD MSIZE
+		MOV DX,BX			;Move to DX the address of the destination memory location = input parameter
+		MOV BX,AX			;Move to BX the file handle that FOPEN returned			   = input parameter
+		FREAD MSIZE			;Read MSIZE bytes from the input file
 		
-		JNC SUCCESS
-		MOV AX,EFREAD
-		JMP CLOSEFILE
+		JNC NR_BYTES		;Check the CF for error
+		MOV AX,EFREAD		;An error has occured on reading the file. Assign the corresponding error code
+		JMP CLOSEFILE		;Jump to the last instructions
+	
+		NR_BYTES:			;FREAD returns the number of bytes read. It needs to be equal with MSIZE.
+		CMP AX,MSIZE		;AX is compared to MSIZE to determine if indeed MSIZE bytes were read.
+		JE	SUCCESS			
+		MOV AX,EFORMAT		;The number of bytes that were read are not the same as MSIZE. Assign the error code.
+		JMP CLOSEFILE		;Jump to the last instructions
 	
 		SUCCESS:
-		MOV AX,ESUCC
+		MOV AX,ESUCC		;The data was successfully read from the file. ESUCC = success
 		
-		CLOSEFILE:
-		PUSH AX
-		FCLOSE
-		POP AX
+		CLOSEFILE:		
+		FCLOSE				;Close the file
 		
 		RETURN:
-		POP BX
+		POP BX				;Restore the registers
 		POP DX
 		RET
 	READ ENDP
